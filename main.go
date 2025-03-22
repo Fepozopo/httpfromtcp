b9d93af061088c60b34bb3a92f366c7f03c70cec
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-03-21 17:56:07 -0700

    Update main.go to read contents of messages.txt

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	// Open messages.txt
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Printf("error opening file: %v", err)
+		return
+	}
+	defer file.Close()
+
+	// Create an empty byte slice
+	buffer := make([]byte, 8)
+
+	// Read the file 8 bytes at a time and print to stdout
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		bufferString := string(buffer[:n])
+		fmt.Printf("read: %s\n", bufferString)
+	}
 }

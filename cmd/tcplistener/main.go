b8fb7404df922dd7b73c77a0939dbf1b3c7fedde
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-03-23 18:32:07 -0700

    Update tcplistener/main.go to work with HTTP instead of just newlines

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/Fepozopo/httpfromtcp/internal/request"
 )
 
 func main() {
@@ -35,61 +35,11 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	lines := getLinesChannel(conn)
-
-	// Loop over the returned channel
-	for line := range lines {
-		fmt.Printf("%s", line)
+	requestLine, err := request.RequestFromReader(conn)
+	if err != nil {
+		log.Printf("error creating request: %v", err)
+		return
 	}
-	fmt.Print("\n") // Print a final terminating newline
 
-	fmt.Println("The connection has been closed")
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	// Channel for each line
-	ch := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(ch)
-
-		buffer := make([]byte, 0, 64) // Buffer to accumulate bytes until a newline is found
-		readBuffer := make([]byte, 8) // Buffer to read 8 bytes at a time
-
-		// Read the file
-		for {
-			n, err := f.Read(readBuffer)
-			if err != nil {
-				if err != io.EOF {
-					log.Printf("error reading: %v", err)
-				}
-				break
-			}
-			// Append the read bytes to the main buffer
-			buffer = append(buffer, readBuffer[:n]...)
-
-			// Process the buffer to find and print complete lines
-			for {
-				// Find the index of the newLine character
-				newLineIndex := bytes.IndexByte(buffer, '\n')
-				if newLineIndex == -1 {
-					break // No newLine found, continue reading
-				}
-
-				// Extract the line and send it to the channel
-				line := string(buffer[:newLineIndex+1])
-				ch <- line
-
-				// Update the buffer to remove the processed line
-				buffer = buffer[newLineIndex+1:]
-			}
-		}
-		// Send any remaining data as the last line
-		if len(buffer) > 0 {
-			line := string(buffer)
-			ch <- line
-		}
-	}()
-	return ch
+	fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n", requestLine.RequestLine.Method, requestLine.RequestLine.RequestTarget, requestLine.RequestLine.HttpVersion)
 }
